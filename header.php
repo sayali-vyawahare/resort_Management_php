70a228619e845a6e37b7a05340dccca1d938a07e
sayali-vyawahare <sayali.v246@gmail.com>
2020-10-15 14:17:36 +0530

    commit

--- header.php
+++ header.php
@@ -1,37 +1,38 @@
-<header>
-    <nav class="navbar navbar-expand-lg navbar-white bg-white">
-        <a class="navbar-brand wow slideInLeft" href="home.php"><img src="assets/img/paradise.png" alt="logo"
-                width="150px" height="auto"></a>
+<header id="home">
+    <nav class="navbar navbar-expand-lg navbar-white">
+        <a class="navbar-brand wow fadeInLeft" href="home.php"><img src="assets/img/paradise.png" alt="logo"
+                height="80px"></a>
         <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav w-100">
-                <li class="nav-item active wow zoomInDown">
-                    <a class="nav-link btn btn-white" href="home.php" role="button" aria-pressed="true">Home</a>
+            <ul class="navbar-nav w-100 mx-auto">
+                <li class="nav-item active wow rotateIn ">
+                    <a class="nav-link btn aqua-gradient color-block mb-3 mx-auto rounded-pill z-depth-1-half"
+                        href="home.php" role="button" aria-pressed="true">Home</a>
                 </li>
-                <li class="nav-item dropdown wow zoomInDown">
-                    <a class="nav-link btn btn-white" href="rooms.php" role="button" aria-pressed="true">Rooms</a>
+                <li class="nav-item dropdown wow rotateIn">
+                    <a class="nav-link btn aqua-gradient color-block mb-3 mx-auto rounded-pill z-depth-1"
+                        href="rooms.php" role="button" aria-pressed="true">Rooms</a>
                 </li>
-                <li class="nav-item wow zoomInDown">
-                    <a class="nav-link btn btn-white" href="#" role="button" aria-pressed="true">Contact</a>
+                <li class="nav-item wow rotateIn">
+                    <a class="nav-link btn aqua-gradient color-block mb-3 mx-auto rounded-pill z-depth-1" href="#"
+                        role="button" aria-pressed="true">Contact</a>
                 </li>
-                <li class="nav-item wow zoomInDown">
-                    <a class="nav-link btn btn-white" href="#" role="button" aria-pressed="true">About</a>
+                <li class="nav-item wow rotateIn">
+                    <a class="nav-link btn aqua-gradient color-block mb-3 mx-auto rounded-pill z-depth-1" href="#"
+                        role="button" aria-pressed="true">About</a>
                 </li>
-                <li class="nav-item wow zoomInDown">
-                    <a class="nav-link btn btn-white wow slideInRight" href="login.php" target="_self" role="button"
-                        aria-pressed="true">User Login</a>
+                <li class="nav-item wow rotateIn">
+                    <a class="nav-link btn aqua-gradient color-block mb-3 mx-auto rounded-pill z-depth-1"
+                        href="login.php" target="_self" role="button" aria-pressed="true">User Login</a>
                 </li>
-                <li class="nav-item wow zoomInDown">
-                    <a class="nav-link btn btn-white wow slideInRight" href="admin/adminlogin.php" target="_self"
-                        role="button" aria-pressed="true">Admin Login</a>
+                <li class="nav-item wow rotateIn">
+                    <a class="nav-link btn aqua-gradient color-block mb-3 mx-auto rounded-pill z-depth-1"
+                        href="admin/adminlogin.php" target="_self" role="button" aria-pressed="true">Admin Login</a>
                 </li>
             </ul>
-            <!-- <form class="form-inline my-1 my-lg-0">
-                    <a class="nav-link wow slideInRight" href="admin/adminlogin.php" target="_self">Admin Login</a>
-                </form> -->
         </div>
     </nav>
 </header>
--- test.php
+++ test.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test</title>
-    <link rel="stylesheet" type="text/css" href="css/test.css">
+    <link rel="stylesheet" type="text/css" href="css/test1.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
@@ -19,37 +19,34 @@
 </head>
 
 <body>
-    <!--Carousel Wrapper-->
-    <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
-        <?php include 'header.php' ?>
+    <?php include 'header.php' ?>
 
-        <!--Indicators-->
+    <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
             <li data-target="#carousel-example-1z" data-slide-to="1"></li>
             <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+            <li data-target="#carousel-example-1z" data-slide-to="3"></li>
+
         </ol>
-        <!--/.Indicators-->
-        <!--Slides-->
         <div class="carousel-inner" role="listbox">
-            <!--First slide-->
             <div class="carousel-item active">
                 <img class="d-block w-100" src="assets/img/resort2.jpg" alt="First slide" height="650px">
+                <div class="carousel-caption caro-capt d-none d-md-block wow slideInUp">
+                    <h3>Welcome to Paradise</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
             </div>
-            <!--/First slide-->
-            <!--Second slide-->
             <div class="carousel-item">
                 <img class="d-block w-100" src="assets/img/resort4.jpg" alt="Second slide" height="650px">
             </div>
-            <!--/Second slide-->
-            <!--Third slide-->
             <div class="carousel-item">
                 <img class="d-block w-100" src="assets/img/resort3.jpg" alt="Third slide" height="650px">
             </div>
-            <!--/Third slide-->
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/resort1.jpg" alt="Third slide" height="650px">
+            </div>
         </div>
-        <!--/.Slides-->
-        <!--Controls-->
         <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
@@ -58,9 +55,293 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
-        <!--/.Controls-->
     </div>
-    <!--/.Carousel Wrapper-->
+    <div class=" class= py-4 text-uppercase text-center ">
+        <h2 class="mt-2 wow zoomIn slow"><u>Rooms</u></h2>
+    </div>
+    <section>
+        <nav class="container-fluid h-100">
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <a class="nav-item nav-link active" id="nav-single-tab" data-toggle="tab" href="#nav-single" role="tab"
+                    aria-controls="nav-single" aria-selected="true">Single Room</a>
+                <a class="nav-item nav-link" id="nav-double-tab" data-toggle="tab" href="#nav-double" role="tab"
+                    aria-controls="nav-double" aria-selected="false">Double Room</a>
+                <a class="nav-item nav-link" id="nav-semideluxe-tab" data-toggle="tab" href="#nav-semideluxe" role="tab"
+                    aria-controls="nav-semideluxe" aria-selected="false">Semideluxe Room</a>
+                <a class="nav-item nav-link" id="nav-deluxe-tab" data-toggle="tab" href="#nav-deluxe" role="tab"
+                    aria-controls="nav-deluxe" aria-selected="true">Deluxe Room</a>
+            </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-single" role="tabpanel" aria-labelledby="nav-single-tab">
+                <div class="container my-2 py-3">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="assets/img/presidential_suite.jpg" width="100%">
+                        </div>
+                        <div class="col-8">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum omnis incidunt aliquid
+                                repudiandae a. Omnis tempore, quidem velit quae eaque blanditiis asperiores impedit,
+                                nobis veritatis voluptatem assumenda cupiditate, totam cum!
+                            </p>
+                            <a class="btn btn-info" href="rooms.php">Check more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="nav-double" role="tabpanel" aria-labelledby="nav-double-tab">
+                <div class="container my-2 py-3">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="assets/img/presidential_suite.jpg" width="100%">
+                        </div>
+                        <div class="col-8">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum omnis incidunt aliquid
+                                repudiandae a. Omnis tempore, quidem velit quae eaque blanditiis asperiores impedit,
+                                nobis veritatis voluptatem assumenda cupiditate, totam cum!</p>
+                            <a class="btn btn-info" href="rooms.php">Check more</a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="nav-semideluxe" role="tabpanel" aria-labelledby="nav-semideluxe-tab">
+                <div class="container my-2 py-3">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="assets/img/presidential_suite.jpg" width="100%">
+                        </div>
+                        <div class="col-8">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum omnis incidunt aliquid
+                                repudiandae a. Omnis tempore, quidem velit quae eaque blanditiis asperiores impedit,
+                                nobis veritatis voluptatem assumenda cupiditate, totam cum!</p>
+                            <a class="btn btn-info" href="rooms.php">Check more</a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="nav-deluxe" role="tabpanel" aria-labelledby="nav-deluxe-tab">
+                <div class="container my-2 py-3">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="assets/img/presidential_suite.jpg" width="100%">
+                        </div>
+                        <div class="col-8">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum omnis incidunt aliquid
+                                repudiandae a. Omnis tempore, quidem velit quae eaque blanditiis asperiores impedit,
+                                nobis veritatis voluptatem assumenda cupiditate, totam cum!</p>
+                            <a class="btn btn-info" href="rooms.php">Check more</a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- facilities -->
+    <div class=" class= py-2 text-uppercase text-center">
+        <h2 class="mt-2 wow zoomIn slow"><u>Featured facilities</u></h2>
+        <div class="row bg-dark m-0">
+            <div class="col-md-3 bg-white p-3 wow slideInLeft  slow">
+                <i class="fa fa-wifi fa-2x" aria-hidden="true"></i>
+                <h4>Free Wifi</h4>
+            </div>
+            <div class="col-md-3 bg-white p-3 wow slideInRight  slow">
+                <i class="fa fa-cutlery fa-2x" aria-hidden="true"></i>
+                <h4>Restaurant</h4>
+            </div>
+            <div class="col-md-3 bg-white p-3 wow slideInLeft  slow">
+                <i class="fa fa-wheelchair-alt fa-2x" aria-hidden="true"></i>
+                <h4>Gym</h4>
+            </div>
+            <div class="col-md-3 bg-white p-3 wow slideInRight  slow">
+                <i class="fa fa-cab fa-2x" aria-hidden="true"></i>
+                <h4>Pick Up</h4>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <!-- Card deck -->
+        <div class="card-deck">
+
+            <!-- Card -->
+            <div class="card mb-4">
+
+                <!--Card image-->
+                <div class="view overlay">
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="assets/img/pla2.jpg" alt="First slide" height="250px">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="assets/img/pla7.jpg" alt="Second slide" height="250px">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="assets/img/pla4.jpg" alt="Third slide" height="250px">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button"
+                            data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button"
+                            data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <a href="#!">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!--Card content-->
+                <div class="card-body">
+
+                    <!--Title-->
+                    <h4 class="card-title">Lorem Ipsum</h4>
+                    <!--Text-->
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit
+                        amet consectetur adipisicing elit.</p>
+                    <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                    <button type="button" class="btn btn-blue btn-md">Read more</button>
+
+                </div>
+
+            </div>
+            <!-- Card -->
+
+            <!-- Card -->
+            <div class="card mb-4">
+
+                <!--Card image-->
+                <div class="view overlay">
+                    <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="assets/img/food1.jpg" alt="First slide" height="250px">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="assets/img/food2.jpg" alt="Second slide" height="250px">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="assets/img/food3.jpg" alt="Third slide" height="250px">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls2" role="button"
+                            data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls2" role="button"
+                            data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <a href="#!">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!--Card content-->
+                <div class="card-body">
+
+                    <!--Title-->
+                    <h4 class="card-title">Lorem Ipsum</h4>
+                    <!--Text-->
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit
+                        amet consectetur adipisicing elit.</p>
+                    <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                    <button type="button" class="btn btn-blue btn-md">Read more</button>
+
+                </div>
+
+            </div>
+            <!-- Card -->
+
+            <!-- Card -->
+            <div class="card mb-4">
+
+                <!--Card image-->
+                <div class="view overlay">
+                    <div id="carouselExampleControls3" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="assets/img/pla1.jpg" alt="First slide" height="250px">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="assets/img/pla6.jpg" alt="Second slide" height="250px">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="assets/img/pla3.jpg" alt="Third slide" height="250px">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls3" role="button"
+                            data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls3" role="button"
+                            data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <a href="#!">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!--Card content-->
+                <div class="card-body">
+
+                    <!--Title-->
+                    <h4 class="card-title">Lorem Ipsum</h4>
+                    <!--Text-->
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit
+                        amet consectetur adipisicing elit.</p>
+                    <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                    <button type="button" class="btn btn-blue btn-md">Read more</button>
+
+                </div>
+
+            </div>
+            <!-- Card -->
+
+        </div>
+        <!-- Card deck -->
+    </div>
+    <!-- video -->
+    <!-- <div class="container">
+        <div class="row">
+            <div class="col-6 bg-light wow">
+                <iframe class="pt-2" width="100%" height="500px" src="https://www.youtube.com/embed/yTCI6yV13kU"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+            </div>
+            <div class="col-6 bg-light">
+                <h3>Lorem ipsum</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti pariatur fugiat, consectetur
+                    quia accusantium reprehenderit harum, deserunt, voluptatum consequuntur optio autem repudiandae
+                    consequatur possimus corporis nostrum alias vitae est iusto?
+                </p>
+            </div>
+        </div> -->
+
+    <footer>
+        <a href="#home" class="toTop scroll"><i class="fa fa-angle-up"></i></a>
+        <p class="text-center">&copy;The paradise 2020</p>
+    </footer>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"
         integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g=="
